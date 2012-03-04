0f47aceb60ba33fa91e36674c42f225c63617c49
Igor <siafin2010@gmail.com>
2012-03-04 22:53:39 +0200

    111

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -8,16 +8,19 @@
 $(document).ready(function(){
     $('.not_current_page_item').mouseenter(function(){
         $(this).animate({
-                        backgroundColor: "#C00000",
+                        backgroundColor: "#803824",
                         color: "#FFFFFF"
                         } , 500);
     });
 
     $('.not_current_page_item').mouseout(function(){
         $(this).animate({
-                        backgroundColor: "#FFFFFF",
-                        color: "#551300"
-                        } , 500);
+                            backgroundColor: "#FFFFFF",
+                            color: "#551300"
+                        } , 500)
+               .queue(function(){
+                                    $(this).queue("fx", [])
+                                });
     });
 
 
