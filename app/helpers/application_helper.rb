4f0f99ec676eb04571603bf6bda40a5b94231180
Igor <siafin2010@gmail.com>
2012-03-23 13:51:51 +0200

    www

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,16 @@ module ApplicationHelper
   def li_current_page_item (selected_page, current_page, &block)
       content = with_output_buffer(&block)
       if selected_page==current_page
-#        content_tag(:span) do
-          content_tag(:li, content, :class => 'current_page_item ui-widget-content', :id=>current_page)
-#        end
+        content_tag(:li, content, :class => 'current_page_item ui-widget-content', :id=>current_page)
       else 
         content_tag(:li, content, :class => 'not_current_page_item ui-widget-content', :id=>current_page)
       end
+  end
+
+  def admin_link_to (*args, &block)
+    if current_user&&current_user.admin?
+      link_to(*args, &block)
+    end
   end
 
 end
