16ddf53b409e58fb640634ba482cd0128ebc4193
Igor <siafin2010@gmail.com>
2012-06-12 17:26:59 +0300

    4

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -22,7 +22,7 @@ $(document).ready(function(){
                                     $(this).queue("fx", [])
                                 });
     });
-
+    
     $('#doctree')
       .jstree({
          "plugins": [
