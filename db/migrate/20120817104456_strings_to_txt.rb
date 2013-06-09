a3104b13f6c081f32265550dd7606372e9903408
ForeverYoung1208 <siafin2010@gmail.com>
2013-06-09 15:07:11 +0300

    fixed migration

--- db/migrate/20120817104456_strings_to_txt.rb
+++ db/migrate/20120817104456_strings_to_txt.rb
@@ -1,12 +1,12 @@
 class StringsToTxt < ActiveRecord::Migration
   def up
     change_table :start_articles do |t|
-      t.change (:content, :text)
+      t.change(:content, :text)
     end
     change_table :services do |t|
-      t.change (:name, :text)
-      t.change (:description, :text)
-      t.change (:price, :text)
+      t.change(:name, :text)
+      t.change(:description, :text)
+      t.change(:price, :text)
     end
   end
 
